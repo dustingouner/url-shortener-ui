6504e7c8ca8d7e46ae9f117c6a8d329574a86519
Dustin Gouner <dustingouner20@gmail.com>
2023-06-12 11:23:06 -0600

    cypress testing for user flows

---
+++ cypress/e2e/home_view_spec.cy.js
@@ -0,0 +1,48 @@
+describe('User Home view', () => {
+  beforeEach(() => {
+    cy.intercept('GET', "http://localhost:3001/api/v1/urls", {
+      fixture: '/urls.json'
+    })
+    cy.visit('http://localhost:3000')
+  })
+
+  
+  it('User should see the page title and a form', () => {
+    cy.get('h1').contains('URL Shortener')
+    cy.get('form')
+      .get('[name="title"]').should('have.value', '')
+      .get('[name="urlToShorten"]').should('have.value', '')
+      .get('button').contains('Shorten Please!')
+
+  })
+
+  it('User should be able to see any existing shortened urls displayed on the screen', () => {
+    cy.get('section')
+      .get('section > :nth-child(1)')
+      .get(':nth-child(1) > h3').contains('Awesome photo')
+      .get(':nth-child(1) > a').contains('http://localhost:3001/useshorturl/1')
+      .get(':nth-child(1) > p').contains('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+      .get('section > :nth-child(2)')
+      .get(':nth-child(2) > h3').contains('new')
+      .get(':nth-child(2) > a').contains('http://localhost:3001/useshorturl/2')
+      .get(':nth-child(2) > p').contains('https://unsplash.com/photos/vsUP_nFAzJ8')
+  })
+
+  it('User can fill out the form and the user inputs are reflected in the input fields', () => {
+    cy.get("form input[name='title']").type("Great Photo")
+    cy.get("form input[name='urlToShorten']").type("https://unsplash.com/photos/m5Wj2ThjA14")
+  })
+
+  it('User can fill in the form, click submit button, and view the new shortened url on the page', () => {
+    cy.get('form')
+      .get("form input[name='title']").type("Great Photo")
+      .get("form input[name='urlToShorten']").type("https://unsplash.com/photos/m5Wj2ThjA14")
+      .get('button').click()
+    cy.get('section')
+      .get('section > :nth-child(3)')
+      .get(':nth-child(3) > h3').contains('Great Photo')
+      .get(':nth-child(3) > a').contains('http://localhost:3001/useshorturl/7')
+      .get(':nth-child(3) > p').contains('https://unsplash.com/photos/m5Wj2ThjA14')
+
+  })
+})
